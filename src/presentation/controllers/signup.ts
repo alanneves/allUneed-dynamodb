556525db0b7e4d7df13adb7fe37c3a3a7bb3449f
Alan Neves <alanrenoneves@gmail.com>
2021-09-01 00:14:11 -0300

    feat: ensure SignUpController returns 500 if emailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { HttpResponse, HttpRequest } from '../protocols/http';
 import MissingParamError from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, internalServerError } from '../helpers/http-helper';
 import Controller from '../protocols/controller';
 import EmailValidator from '../protocols/email-validator';
 import InvalidParamError from '../errors/invalid-param-error';
+import ServerError from '../errors/server-error';
 
 export default class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -13,19 +14,21 @@ export default class SignUpController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
-    // eslint-disable-next-line no-restricted-syntax
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      // eslint-disable-next-line no-restricted-syntax
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
+      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isEmailValid) return badRequest(new InvalidParamError('email'));
 
-    if (!isEmailValid) {
-      return badRequest(new InvalidParamError('email'));
+      return badRequest(new MissingParamError('a'));
+    } catch (error) {
+      return internalServerError(new ServerError());
     }
-    return badRequest(new MissingParamError('a'));
   }
 }
