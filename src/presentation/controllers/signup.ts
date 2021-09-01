690557180b85c1b8b020e8eddac9fcba149cf758
Alan Neves <alanrenoneves@gmail.com>
2021-09-01 00:19:18 -0300

    refactor: remove unusable import

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,6 @@ import { badRequest, internalServerError } from '../helpers/http-helper';
 import Controller from '../protocols/controller';
 import EmailValidator from '../protocols/email-validator';
 import InvalidParamError from '../errors/invalid-param-error';
-import ServerError from '../errors/server-error';
 
 export default class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
